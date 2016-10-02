217ac0ab16d91c2c2fe476409ec17ff2ffe245b7
domenge <dc@macarel.net>
2016-10-02 17:09:59 +0200

    correction

--- php/connexion.php
+++ php/connexion.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("localhost","drupal","ch0c0l\@t","labo.macarel.net");
+$con = new mysqli("localhost","drupal","mypasswd","labo.macarel.net");
 $result = $con->query("SELECT term,id FROM totenun_OF' LIMIT 0,10");
     while ($row = $result->fetch_object()){
          $user_arr[] = $row->id;
